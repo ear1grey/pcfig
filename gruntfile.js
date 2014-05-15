4654aeedfa5bad15990dc7c92d5ed5017406fdaf
ear1grey <rjb@port.ac.uk>
2014-05-15 01:29:00 +0100

    adding main files

---
+++ gruntfile.js
@@ -0,0 +1,78 @@
+/*global module */
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded',
+					lineNumbers: true,
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= pkg.folders.src %>/',
+					src: ['*.scss'],
+					dest: '<%= pkg.folders.build %>',
+					ext: '.css',
+				}]
+			}
+		},
+
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {
+                        expand: true,
+                        cwd: '<%= pkg.folders.src %>',
+                        src: [
+                            '**/*.html',
+                            '**/*.svg'
+                        ],
+                        dest: '<%= pkg.folders.build %>/',
+                        filter: 'isFile',
+                        flatten: false
+                    }
+
+                ]
+            }
+        },
+
+        htmlSnapshot: {
+            all: {
+              options: {
+                snapshotPath: '<%= pkg.folders.build %>/g/',
+                sitePath: 'file:///Users/rjb/Dropbox/code/pcfig/build/index.html#',
+                urls: ['']
+              }
+            }
+        },
+
+        watch: {
+            files: ['<%= pkg.folders.src %>/**/*'],
+            tasks: ['default'],
+            options: {
+                livereload: true
+            }
+        },
+
+        'gh-pages': {
+            options: {
+                base: '<%= pkg.folders.build %>'
+            },
+            src: ['**']
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-html-snapshot');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+    grunt.registerTask('default', ['sass', 'copy']);
+
+};
